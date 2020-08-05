08ca9f105b0ddca49b575694ea11963047379bb4
Matoi07 <50721247+Matoi07@users.noreply.github.com>
2020-08-05 20:32:52 +0700

    Add files via upload

---
+++ src/bangundatar/strategy/LayangLayang.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bangundatar.strategy;
+import java.util.Scanner;
+/**
+ *
+ * @author Adhitya Dwi Cahyana
+ */
+public class LayangLayang implements Strategy{
+    @Override
+    public String hitung(int hasil){
+        return "" +hasil;
+    }
+}
